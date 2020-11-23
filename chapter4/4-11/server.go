46800d702d8f79db8815ecf875166566767c426e
satoshi <hase.sato.8888@gmail.com>
2020-11-23 22:18:50 +0900

    webapp

---
+++ chapter4/4-11/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Post struct {
+	User    string
+	Threads []string
+}
+
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+	<head><title>Go Web Programing</title></head>
+	<body><h1>Hello World
+	<h2>Wow</h2>
+	</body>
+	</html>`
+	w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "そのようなサービスはありません．他を当たってください")
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sou Sou",
+		Threads: []string{"一番目", "二番目", "三番目"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
+	server.ListenAndServe()
+}
