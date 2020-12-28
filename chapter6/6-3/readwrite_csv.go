2350a521bf11a587c167fcc217166a2edba28748
satoshi <hase.sato.8888@gmail.com>
2020-12-28 14:24:00 +0900

    write csv

---
+++ chapter6/6-3/readwrite_csv.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Post struct {
+	ID      int
+	Content string
+	Author  string
+}
+
+func main() {
+	csvFile, err := os.Create("posts.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	allPosts := []Post{
+		Post{ID: 1, Content: "I like sushi!", Author: "sushiro"},
+		Post{ID: 2, Content: "I like fried chicken!", Author: "Colonel Sanders"},
+		Post{ID: 3, Content: "I like hamburger!", Author: "McDonald"},
+		Post{ID: 4, Content: "I like ramen!", Author: "jiro"},
+	}
+
+	writer := csv.NewWriter(csvFile)
+	for _, post := range allPosts {
+		line := []string{strconv.Itoa(post.ID), post.Content, post.Author}
+		err := writer.Write(line)
+		if err != nil {
+			panic(err)
+		}
+	}
+	writer.Flush()
+
+	file, err := os.Open("posts.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader((file))
+	reader.FieldsPerRecord = -1
+	record, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var posts []Post
+	for _, item := range record {
+		id, _ := strconv.ParseInt(item[0], 0, 0)
+		post := Post{ID: int(id), Content: item[1], Author: item[2]}
+		posts = append(posts, post)
+	}
+
+	fmt.Println(posts[0].ID)
+	fmt.Println(posts[0].Content)
+	fmt.Println(posts[0].Author)
+
+}
