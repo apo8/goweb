5b26e8ed74a52577678778024531d9163f926ab0
satoshi <hase.sato.8888@gmail.com>
2021-02-20 17:19:59 +0900

    gorm

--- chapter6/6-5-2/store.go
+++ chapter6/6-5-2/store.go
@@ -1,13 +1,50 @@
 package main
 
-type Post struct{
-	Id int
-	Content string
-	Author string `"sql:not null"`
-	Comment []Comment
+import (
+	"fmt"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	Id        int
+	Content   string
+	Author    string `"sql:not null"`
+	Comments  []Comment
 	CreatedAt time.Time
 }
 
+type Comment struct {
+	Id        int
+	Content   string
+	Author    string `sql:"not null"`
+	PostId    int
+	CreatedAt time.Time
+}
+
+var Db *gorm.DB
+
+func init() {
+	var err error
+	Db, err = gorm.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	Db.AutoMigrate(&Post{}, &Comment{})
+}
+
 func main() {
-	
+	post := Post{Content: "Hello world", Author: "Sara"}
+	fmt.Println(post)
+	Db.Create(&post)
+	fmt.Println(post)
+	comment := Comment{Content: "good news!", Author: "Joe"}
+	Db.Model(&post).Association("Comments").Append(comment)
+	var readPost Post
+	Db.Where("author=$1", "Sara").First(&readPost)
+	var comments []Comment
+	Db.Model(&readPost).Related(&comments)
+	fmt.Println(comments[0])
 }
